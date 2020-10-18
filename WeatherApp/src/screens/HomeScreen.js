e483c0a8a86bd66021f8a9c06626bae7e69edd82
Tanvir Singh <tanvir.singh@kelltontech.com>
2020-10-18 23:49:40 +0530

    Home screen UI started

--- WeatherApp/src/screens/HomeScreen.js
+++ WeatherApp/src/screens/HomeScreen.js
@@ -1,12 +1,75 @@
 import React, {PureComponent} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+
+
+const arr = [
+    {
+      day: 'Monday',
+      temp: 8,
+    },
+    {
+      day: 'Monday',
+      temp: 8,
+    },
+    {
+      day: 'Monday',
+      temp: 8,
+    },
+    {
+      day: 'Monday',
+      temp: 8,
+    },
+    {
+      day: 'Monday',
+      temp: 8,
+    },
+  ];
 
 export default class HomeScreen extends PureComponent {
-  render() {
-    return (
-      <View style={{flex:1, justifyContent:'center', alignItems: 'center'}}>
-          <Text>HomeScreen</Text>
-      </View>
-    );
-  }
+
+
+    renderCard = (obj) => {
+        console.log('object', obj);
+        return (
+            <View style={styles.cardView}>
+                <Text style={{fontSize: 22}}>{obj.item.day}</Text>
+                <Text style={{fontSize: 22}}>{obj.item.temp}</Text>
+            </View>
+        );
+    };
+    
+    
+    render() {
+        return (
+        <View style={styles.container}>
+            <View style={{ backgroundColor:'white', flex:1}}>
+                <View style={{justifyContent:'center', alignItems:'center', flex: 1, backgroundColor: 'blue'}}>
+                    <Text>10</Text>
+                    <Text>Delhi</Text>                                                  
+                </View>
+                <View>
+                    <FlatList data={arr} renderItem={(item) => this.renderCard(item)} />
+                </View>
+            </View>
+        </View>
+        );
+    };
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1, 
+        justifyContent:'center',
+        backgroundColor: 'blue'
+    },
+    cardView:{
+        justifyContent: 'space-between', 
+        alignItems: 'center',
+        flexDirection: 'row', 
+        backgroundColor:'white', 
+        paddingHorizontal: 30, 
+        height:50,
+        borderTopWidth: 1, 
+        borderTopColor: 'black'
+    }
+})
